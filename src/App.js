24f3817c4eb9b710f47b4a48d25b5718c8133da8
Jose Santiago <josantorres93@gmail.com>
2020-04-12 22:05:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const Profile = () => {
+  // const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({ loading: true });
+  const url = 'https://randomuser.me/api/';
+
+  useEffect(() => {
+    const getUser = async() => {
+      const response = await fetch(url);
+      const data = await response.json();
+      const userData = data.results[0];
+      const name = `${userData.name.first} ${userData.name.last}`;
+      const picture = userData.picture.large;
+      setUser({ name: name, picture: picture });
+    }
+
+    getUser();
+    
+  }, []);
+
+  return(
+    <div>
+      {
+        user.loading ? 'Loading...' : 
+        (<div>
+          <div>{user.name}</div>
+          <img src={user.picture}></img>
+        </div>
+        )
+      }
+    </div>
+  )
+}
+
+const ProfileGrid = () => {
+  const [profiles, setProfiles] = useState([]);
+  for (let i = 0; i < 10; i++) {
+    setProfiles([...profiles, <Profile/>]);
+  }
+  console.log(`profiles:`);
+  console.log(profiles);
+  // return (
+  //   profiles.map((profile) => {
+      
+  //   })
+  // )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        PhotoLocker
       </header>
+      <main>
+        <Profile/>
+        <ProfileGrid/>
+      </main>
     </div>
   );
 }
